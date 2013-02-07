7cb061c96ed7b42c98ba3eead80b33ef626b13b3
brandon ewoldt <brandonewoldt@brandon-ewoldts-iMac.local>
2013-02-07 07:49:41 -0600

    changes

--- about.php
+++ about.php
@@ -13,38 +13,46 @@
     </div>
     <div class="container main-content">
         <img src="images/about_us.jpg" alt="" />
-        <h1 class="text-center">About Us</h1>
         <div class="row-fluid">
-            <div class="span6">
-                <h4>Mission</h4>
-                <p>EWB-USA supports community-driven development programs worldwide by collaborating with local partners to design and implement sustainable engineering projects, while creating transformative experiences and responsible leaders.</p>
+            <div class="span10 offset1">
+                <h1 class="text-center">About Us</h1>
+                <div class="text-center">
+                    <h4>Mission</h4>
+                    <p>EWB-USA supports community-driven development programs worldwide by collaborating with local partners to design and implement sustainable engineering projects, while creating transformative experiences and responsible leaders.</p>
+                    <h4>Vision</h4>
+                    <p>The mission of this student chapter is to partner with villages in the Ejura Sekyedumase District in Ghana, West Africa, on global health projects focused mainly on community drinking water and sanitation. Our in-country partner for this project is Self-Help International based in Waverly, Iowa. UIEWB is committed to partner with the people of Ghana through the year 2015, but we hope to continue the partnership past then.</p>
+                </div>
+                <br>
+                <br>
+                <div class="panel clearfix">
+                    <img src="http://placehold.it/200x300&amp;text=[img]" class="right" />
+                    <h4>Hancher Finkbine Medallion</h4>
+                    <p>2009-2010 UIEWB President Mike Schaefer was awarded a Distinguished Student Leader Certificate by the Hancher-Finkbine Medallion Committee to recognize meritorious qualities in leadership, learning and loyalty.</p>
+                    <h4>Diversity Catalyst Award - 2010</h4>
+                    <p>The student of Engineers Without Borders were awarded he Catalyst Awards, which are designed to honor those who have shown a commitment to achieving excellence through diversity at the University of Iowa.</p>
+                </div>
+                <h2 class="text-center">Current Members 2011-12</h2>
+                <br>
+                <div class="row-fluid">
+                    <div class="span6">
+                        <div class="staff-img"><img src="images/staff/brianna.jpg" class="member-img left" /></div>
+                        <p><strong>Brianna Knoll</strong><br><small>President</small></p>
+                        <p>Brianna is a senior biomedical engineering student who has been a part of EWB since the first semester of her freshman year and since then has run two of the group's fundraisers, traveled to the village the organization supports, and served as vice-president. Brianna joined EWB because, "It is an organization that does not just try to fix problems in developing countries but works with the communities so they have ownership over implementing the solution. It makes what we do sustainable and allows us to truly get to know these wonderful people we are helping out." She hopes to attend medical school after graduation.</p>
+                    </div>
+                    <div class="span6">
+                        <img src="images/staff/adrianna.jpg" class="member-img left" />
+                        <p><strong>Adrianna Jarosz</strong><br><small>Vice President</small></p>
+                        <p>Adrianna is a senior civil and environmental engineering student with an emphasis on water quality. She has been a member of EWB since her sophomore year and has been heavily involved in the design of a water tower for Kobriti, Ghana. In the summer of 2012, she traveled to Nicaragua with friends to build a bridge through Bridges to Prosperity. She also enjoys traveling, cooking, (eating), 30 Rock, and racquetball. After graduation, she hopes to continue exploring the links between engineering and positive change.</p>
+                    </div>
+                </div><!-- end .row -->
             </div>
-            <div class="span6">
-                <h4>Vision</h4>
-                <p>The mission of this student chapter is to partner with villages in the Ejura Sekyedumase District in Ghana, West Africa, on global health projects focused mainly on community drinking water and sanitation. Our in-country partner for this project is Self-Help International based in Waverly, Iowa. UIEWB is committed to partner with the people of Ghana through the year 2015, but we hope to continue the partnership past then.</p>
-            </div>
-        </div><!-- end .row -->
-        <div class="panel clearfix">
-            <img src="http://placehold.it/200x300&amp;text=[img]" class="right" />
-            <h4>Hancher Finkbine Medallion</h4>
-            <p>2009-2010 UIEWB President Mike Schaefer was awarded a Distinguished Student Leader Certificate by the Hancher-Finkbine Medallion Committee to recognize meritorious qualities in leadership, learning and loyalty.</p>
-            <h4>Diversity Catalyst Award - 2010</h4>
-            <p>The student of Engineers Without Borders were awarded he Catalyst Awards, which are designed to honor those who have shown a commitment to achieving excellence through diversity at the University of Iowa.</p>
         </div>
-        <h2 class="text-center">Current Members 2011-12</h2>
-        <br>
-        <div class="row-fluid">
-            <div class="span6">
-                <div class="staff-img"><img src="images/staff/brianna.jpg" class="member-img left" /></div>
-                <p><strong>Brianna Knoll</strong><br><small>President</small></p>
-                <p>Brianna is a senior biomedical engineering student who has been a part of EWB since the first semester of her freshman year and since then has run two of the group's fundraisers, traveled to the village the organization supports, and served as vice-president. Brianna joined EWB because, "It is an organization that does not just try to fix problems in developing countries but works with the communities so they have ownership over implementing the solution. It makes what we do sustainable and allows us to truly get to know these wonderful people we are helping out." She hopes to attend medical school after graduation.</p>
-            </div>
-            <div class="span6">
-                <img src="images/staff/adrianna.jpg" class="member-img left" />
-                <p><strong>Adrianna Jarosz</strong><br><small>Vice President</small></p>
-                <p>Adrianna is a senior civil and environmental engineering student with an emphasis on water quality. She has been a member of EWB since her sophomore year and has been heavily involved in the design of a water tower for Kobriti, Ghana. In the summer of 2012, she traveled to Nicaragua with friends to build a bridge through Bridges to Prosperity. She also enjoys traveling, cooking, (eating), 30 Rock, and racquetball. After graduation, she hopes to continue exploring the links between engineering and positive change.</p>
-            </div>
-        </div><!-- end .row -->
+
+        
+        
+        
+        
+        
         <hr>
         <div class="row-fluid">
             <div class="span6">
