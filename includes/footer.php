b3ff71dd7b9dafc743abf48ee5b80547264f2d90
brandon ewoldt <brandonewoldt@brandon-ewoldts-iMac.local>
2013-02-12 07:05:38 -0600

    new

--- includes/footer.php
+++ includes/footer.php
@@ -1,22 +1,7 @@
-<!-- <div id="flex-footer" class="flexslider">
-    <ul class="slides">
-        <li><img src="http://placehold.it/150x150&amp;text=1" /></li>
-        <li><img src="http://placehold.it/150x150&amp;text=2" /></li>
-        <li><img src="http://placehold.it/150x150&amp;text=3" /></li>
-        <li><img src="http://placehold.it/150x150&amp;text=4" /></li>
-        <li><img src="http://placehold.it/150x150&amp;text=5" /></li>
-        <li><img src="http://placehold.it/150x150&amp;text=6" /></li>
-        <li><img src="http://placehold.it/150x150&amp;text=7" /></li>
-        <li><img src="http://placehold.it/150x150&amp;text=8" /></li>
-        <li><img src="http://placehold.it/150x150&amp;text=9" /></li>
-        <li><img src="http://placehold.it/150x150&amp;text=10" /></li>
-    </ul>
-</div> -->
 <footer class="footer">
     <div class="wrapper">
         <div class="container">
-            <p>test</p>
-            <p>test</p>
+
         </div>
     </div>
 </footer>
